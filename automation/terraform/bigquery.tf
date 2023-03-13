c18be5cc0ed5b0194fd930dc3b698b6f4f21da79
Wiktor Zolnierowicz <w.zolnierowicz@gmail.com>
2023-03-13 17:43:56 +0100

    tf - auto creation of bg data set

---
+++ automation/terraform/bigquery.tf
@@ -0,0 +1,6 @@
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "mds_test_dataset" # reference this in .env
+  friendly_name               = "mds test dataset"
+  description                 = "This is a test dataset"
+  location                    = "US"
+}
